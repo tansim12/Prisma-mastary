b58347f16225dfe8ccfd09fa6be2534dd09b089f
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-01 16:47:27 +0600

    aggregate

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,20 @@
 import { PrismaClient, UserRole } from "@prisma/client";
-import { emit } from "process";
-import { inflate } from "zlib";
 
-const prisma = new PrismaClient({
-  log:[
-   {
-    emit:"event",
-    level:"query"
-   }
-  ]
-});
-prisma.$on("query",(e)=>{
-  console.log(e);
-  
-})
+
+const prisma = new PrismaClient();
 
 const main = async () => {
-  const filteringByAnd = await prisma.post.findMany({
-    where: {
-      AND: [
-        {
-          title: {
-            contains: "sub",
-          },
-        },
-        {
-          publish: true,
-        },
-      ],
+  const aggregateFn = await prisma.user.aggregate({
+    _avg: {
+      age: true,
     },
-    include: {
-      postCategory: {
-        include:{
-          category:true
-        }
-      },
-    },
+    
   });
 
-  // const filteringByOr = await prisma.post.findMany({
-
-  //   where: {
-  //     OR: [
-  //       {
-  //         title: {
-  //           contains: "sub",
-  //         },
-  //       },
-  //       {
-  //         publish: false,
-  //       },
-  //     ],
-  //   },
-  // });
-  // console.dir(filteringByAnd, { depth: inflate });
+  // find number of record 
+  const countRecordByFields = await prisma.user.count()
+  console.log(aggregateFn);
+  
 };
 
 main();
